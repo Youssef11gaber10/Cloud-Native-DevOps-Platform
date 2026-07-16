4dd21e3413159f1954c0a6c39795785951491b8e
youssef gaber <youssef11gaber10@gmail.com>
2026-07-16 14:45:15 +0300

    add app-infra

--- Infra/app-infra/EKS-cluster/output.tf
+++ Infra/app-infra/EKS-cluster/output.tf
@@ -12,5 +12,5 @@ output "cluster_endpoint" {
 }
 
 output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+  value = module.eks.cluster_certificate_authority_dat
 }
