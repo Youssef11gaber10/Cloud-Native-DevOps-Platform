443badd9654d017c66fac9a54bf78c2b554f092b
youssef gaber <youssef11gaber10@gmail.com>
2026-07-28 09:51:57 +0300

    addd application set for applicatoin

--- Code/front-end/src/Components/Shared/baseUrl.js
+++ Code/front-end/src/Components/Shared/baseUrl.js
@@ -1,4 +1,4 @@
-const apiHost = process.env.REACT_APP_API_HOST || 'api';
+const apiHost = process.env.REACT_APP_API_HOST || '/api';
 // const apiPort = process.env.REACT_APP_API_PORT || '5000';
 
 // const baseUrl = `${apiHost}:${apiPort}`;
